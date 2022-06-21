e430de1b1bcf894ae7cc685815ec4840c3240628
thalesAlves758 <thalesalves758@gmail.com>
2022-06-21 08:32:24 -0300

    Adicionar metodo post

--- src/main/java/com/example/lp3/api/controller/DescontoController.java
+++ src/main/java/com/example/lp3/api/controller/DescontoController.java
@@ -3,14 +3,19 @@ import com.example.lp3.api.dto.DescontoDTO;
 import com.example.lp3.model.entity.Desconto;
 import com.example.lp3.service.DescontoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -33,5 +38,21 @@ public class DescontoController {
             return new ResponseEntity("Desconto não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(desconto.map(DescontoDTO::create));
+    }
+
+    @PostMapping
+    public ResponseEntity post(DescontoDTO dto) {
+        try {
+            Desconto desconto = converter(dto);
+            desconto = service.salvar(desconto);
+            return new ResponseEntity(desconto, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Desconto converter(DescontoDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(dto, Desconto.class);
     }
 }
