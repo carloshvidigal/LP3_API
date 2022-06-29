ae07933bb6311754a962979dd63ceb5e7f8a4dce
thalesAlves758 <thalesalves758@gmail.com>
2022-06-28 23:55:03 -0300

    Criar métodos post, put e delete

--- src/main/java/com/example/lp3/api/controller/ProdutoController.java
+++ src/main/java/com/example/lp3/api/controller/ProdutoController.java
@@ -1,17 +1,17 @@
 package com.example.lp3.api.controller;
 
-import com.example.lp3.api.dto.CargoDTO;
+import com.example.lp3.api.dto.FabricanteDTO;
 import com.example.lp3.api.dto.ProdutoDTO;
-import com.example.lp3.model.entity.Cargo;
+import com.example.lp3.exception.RegraNegocioException;
+import com.example.lp3.model.entity.Fabricante;
 import com.example.lp3.model.entity.Produto;
 import com.example.lp3.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +21,9 @@ import java.util.stream.Collectors;
 @RequestMapping("api/v1/produtos")
 @RequiredArgsConstructor
 public class ProdutoController {
+    @Autowired
+    private ModelMapper modelMapper;
+
     private final ProdutoService service;
 
     @GetMapping
@@ -36,5 +39,50 @@ public class ProdutoController {
             return new ResponseEntity("Produto não encontrado", HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(produto.map(ProdutoDTO::create));
+    }
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody ProdutoDTO dto) {
+        try {
+            Produto produtoRequisicao = modelMapper.map(dto, Produto.class);
+            Produto produto = service.salvar(produtoRequisicao);
+
+            ProdutoDTO produtoResposta = modelMapper.map(produto, ProdutoDTO.class);
+            return new ResponseEntity(produtoResposta, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity put(@PathVariable("id") Long id, @RequestBody ProdutoDTO dto) {
+        if(!service.getProdutoById(id).isPresent()) {
+            return new ResponseEntity("Produto não encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            Produto produtoRequisicao = modelMapper.map(dto, Produto.class);
+            produtoRequisicao.setId(id);
+            Produto produto = service.salvar(produtoRequisicao);
+
+            ProdutoDTO produtoResposta = modelMapper.map(produto, ProdutoDTO.class);
+            return ResponseEntity.ok(produtoResposta);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        Optional<Produto> produto = service.getProdutoById(id);
+        if (!produto.isPresent()) {
+            return new ResponseEntity("Produto não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(produto.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
