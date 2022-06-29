b6002728609cc918ea9e8b4523c60c810e928b8e
thalesAlves758 <thalesalves758@gmail.com>
2022-06-29 16:30:45 -0300

    Criar método post e editar método put

--- src/main/java/com/example/lp3/api/controller/CompraController.java
+++ src/main/java/com/example/lp3/api/controller/CompraController.java
@@ -6,8 +6,11 @@ import com.example.lp3.exception.RegraNegocioException;
 import com.example.lp3.model.entity.Compra;
 import com.example.lp3.model.entity.Permissao;
 import com.example.lp3.service.CompraService;
+import com.example.lp3.service.ItemCompraService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +18,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/v1/compras")
 @RequiredArgsConstructor
 public class CompraController {
+    @Autowired
+    private ModelMapper modelMapper;
+
     private final CompraService service;
 
     @GetMapping()
@@ -35,17 +42,33 @@ public class CompraController {
         }
         return ResponseEntity.ok(compra.map(CompraDTO::create));
     }
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody CompraDTO dto) {
+        try {
+            Compra compraRequisicao = modelMapper.map(dto, Compra.class);
+            Compra compra = service.salvar(compraRequisicao);
+
+            CompraDTO compraResposta = modelMapper.map(compra, CompraDTO.class);
+            return new ResponseEntity(compraResposta, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
     @PutMapping("{id}")
-    public ResponseEntity put(@PathVariable("id") Long id, CompraDTO dto) {
+    public ResponseEntity put(@PathVariable("id") Long id, @RequestBody CompraDTO dto) {
         if(!service.getCompraById(id).isPresent()) {
             return new ResponseEntity("Compra não encontrada", HttpStatus.NOT_FOUND);
         }
 
         try {
-            Compra  compra  = converter(dto);
-            compra.setId(id);
-            service.salvar(compra);
-            return ResponseEntity.ok(compra);
+            Compra compraRequisicao = modelMapper.map(dto, Compra.class);
+            compraRequisicao.setId(id);
+            Compra compra = service.salvar(compraRequisicao);
+
+            CompraDTO compraResposta = modelMapper.map(compra, CompraDTO.class);
+            return ResponseEntity.ok(compraResposta);
         } catch (RegraNegocioException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -63,10 +86,5 @@ public class CompraController {
         } catch (RegraNegocioException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-    }
-
-    public Compra  converter(CompraDTO dto) {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper.map(dto, Compra.class);
     }
 }
