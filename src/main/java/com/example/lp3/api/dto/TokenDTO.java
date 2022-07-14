3832895f71b3830b7dc4b6b8492d0319888c9f9d
carloshvidigal <82422474+carloshvidigal@users.noreply.github.com>
2022-07-14 15:20:52 -0300

    Create JwtService, JwtAuthFilter and TokenDTO

---
+++ src/main/java/com/example/lp3/api/dto/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.example.lp3.api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+    private String login;
+    private String token;
+}
